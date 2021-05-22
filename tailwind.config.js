64e3bf5ce59c6a83924db1dd8eaf5c24287e1abf
Parthipan-Natkunam <natkunamparthipan@gmail.com>
2021-05-22 21:46:11 +0530

    setup project with tailwind, craco and daisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safeList: [/data-theme$/],
+    },
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["emerald"],
+    resets: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
